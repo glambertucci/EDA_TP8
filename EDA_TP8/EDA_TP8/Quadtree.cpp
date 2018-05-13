b335c44b08b035f8d36e439f91de24cc95c2689f
Lucas <ldellisola@itba.edu.ar>
2018-05-13 20:25:25 -0300

    added quadtree Enconder

---
+++ EDA_TP8/EDA_TP8/Quadtree.cpp
@@ -0,0 +1,5 @@
+#include "Quadtree.h"
+
+void quadtreeEncoder(ostream & output, float x, float y, float lenght, char ** rawPNG)
+{
+}
---
+++ EDA_TP8/EDA_TP8/Quadtree.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <fstream>
+
+
+using namespace std;
+
+void quadtreeEncoder(ostream& output, float x, float y, float lenght, char ** rawPNG );
