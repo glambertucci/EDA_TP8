163b57fa9823f1d49db27720b5f2c5b740a778ba
Lucas <ldellisola@itba.edu.ar>
2018-05-13 23:07:33 -0300

    added mosaic

---
+++ EDA_TP8/EDA_TP8/Mosaic.cpp
@@ -0,0 +1,12 @@
+#include "Mosaic.h"
+
+
+
+Mosaic::Mosaic()
+{
+}
+
+
+Mosaic::~Mosaic()
+{
+}
---
+++ EDA_TP8/EDA_TP8/Mosaic.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+
+using namespace std;
+
+class Mosaic 
+{
+public:
+	Mosaic();
+	~Mosaic();
+private:
+	string path;
+	int nameStart;
+	bool selected;
+
+};
+
